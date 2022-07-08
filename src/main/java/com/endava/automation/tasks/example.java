35e018b25c71098811940018d2001589b9f3d7e7
Dsendava <daniel.sanchez@endava.com>
2022-07-08 07:24:54 -0500

    example

---
+++ src/main/java/com/endava/automation/tasks/example.java
@@ -0,0 +1,14 @@
+package com.endava.automation.tasks;
+
+import com.microsoft.playwright.*;
+
+public class example {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}
